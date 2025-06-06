61d18d17b32784fa6c7e434756cfef77671b3417
alanasrocha08 <alanasr02@gmail.com>
2025-06-06 10:37:39 -0300

    Adiciona mini-exercício

--- 03-funcoes.js
+++ 03-funcoes.js
@@ -25,4 +25,24 @@ exemplo2(); //chamada (sempre apó declarar a função)
 
 separador();
 
-/* */
+//Mini exercícios:
+/*1) Crie uma função (qualqier sintaxe) que receba dois valores numéricos como parâmetro, calcule a diferença entre estes vslores e retorne o resultado calculado.*/
+function calcularDiferenca(valor1, valor2){
+    return valor1 - valor2;
+}
+
+separador();
+
+/*2) Chame esta função pelo menos duas vezes passando valores diferentes e guardando os resultasdos en variáveis de escopo global. */
+let resposta = calcularDiferenca(7,2);
+let resposta2 = calcularDiferenca(9,3);
+
+
+/*3) Apresente os resultados do console. */
+let mensagem1 = "O resultado é "+resposta;
+console.log(mensagem1);
+
+let mensagem2 = "O resultado é "+resposta2; 
+console.log(mensagem2);
+
+
