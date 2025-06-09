ac9fa5251fa6638d06750fc8c12cacb8ca8e7f4e
alanasrocha08 <alanasr02@gmail.com>
2025-06-09 10:36:53 -0300

    Adiciona importação com mesmo nome usando apelido

--- 05-usando-modulos-esm.js
+++ 05-usando-modulos-esm.js
@@ -6,6 +6,9 @@ import alunos from './modulos-esm/alunos.js';
 //Importação de múltiplos recursos/módulos usando ESM
 import { saudacao, clientes, separador } from './modulos-esm/exemplo.js';
 
+//Uso de um 'apelido/alias' para evitar conflito de nomes
+import { pessoa, livro, alunos as personagens } from './modulos-esm/dados.js';
+
 saudacao("Nathalya Suguiura");
 console.log(clientes);
 
@@ -18,3 +21,24 @@ console.log(alunos);
 for(const aluno of alunos){
     console.log(aluno);
 }
+
+separador();
+
+//Acessando modulo 'dados'
+console.log(pessoa.nome);
+console.log(livro);
+console.log(personagens); // Lembre-se: personagens é um apleido para alunos 
+
+/*Exercícios 
+1) Crie um arquivo chmado "funcoes.js" dentro da pasta "modulis-esm"
+2) Dentro dele, programe 3 funções: 
+    - Converter caracteres para MAIÚCULO
+    - Converter caracteres para MINÚSCULO
+    - Formatar valor em oeda brasileira (real) 
+    
+3) Exporte-as como módulos 
+4) No arquivo 05, importe e utilize estas funções passando textor e valores para testes.*/
+
+
+
+
---
+++ modulos-esm/dados.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const pessoa = {
+    nome: 'Alana Silva Rocha',
+    idade: 17,
+    cidade: 'São Paulo'
+};
+
+const livro = {
+    titulo: 'Harry Potter',
+    volume: "A camara secreta",
+    ano: 1998
+};
+
+const alunos = ['Clara', 'Julia', 'kauê', 'Nathalya'];
+
+export { pessoa, livro, alunos };
