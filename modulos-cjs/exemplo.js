d241c5ef1eb2df0d6050e92816cdf5e0b0875b6e
alanasrocha08 <alanasr02@gmail.com>
2025-06-09 09:04:21 -0300

    Importa os módulos usando CJS

--- modulos-cjs/exemplo.js
+++ modulos-cjs/exemplo.js
@@ -4,7 +4,7 @@ function minhaFuncao(){
     console.log("Olá! Função criada em uma módulo");
 }
 
-const clientes = ['Alana Rocha', 'Clara Igrja', 'Julia Moura'];
+const clientes = ['Alana Rocha', 'Clara Igreja', 'Julia Moura'];
 
 /*CJS: CommonJS */
 module.exports = { minhaFuncao, clientes };
