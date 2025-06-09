52bf129fa7bb5efe0e5f814db9ba25f0ccd49f26
alanasrocha08 <alanasr02@gmail.com>
2025-06-09 09:38:50 -0300

    Adiciona função separador no arquivo 05

--- 05-usando-modulos-esm.js
+++ 05-usando-modulos-esm.js
@@ -1,7 +1,13 @@
 'use strict';
 
+// Importação de recurso padrão (default)
+import alunos from './modulos-esm/alunos.js';
+
 //Importação de múltiplos recursos/módulos usando ESM
-import { saudacao, clientes } from './modulos-esm/exemplo.js';
+import { saudacao, clientes, separador } from './modulos-esm/exemplo.js';
 
 saudacao("Nathalya Suguiura");
 console.log(clientes);
+
+separador();
+
--- modulos-esm/exemplo.js
+++ modulos-esm/exemplo.js
@@ -1,10 +1,15 @@
 'use strict';
 
+function separador(){
+    console.log("\n************************");
+}
+
 /*Módulos ESM: EcmaScript Modules (sistema atual, moderno) */
 function saudacao(nome){
     console.log("Olá " + nome + "!");
 }
 
 const clientes = ['Nathalya Suguiura', 'Kauê Martins', 'Vitor Alexandre']; 
+
 // Colocando entre chaves, é para exportar um objeto
 export { saudacao, clientes }; 
