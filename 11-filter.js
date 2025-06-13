244f902d700b2c48bf58d9abb59a2df80a9ed3fd
alanasrocha08 <alanasr02@gmail.com>
2025-06-13 11:14:52 -0300

    Adiciona exercício com filter

--- 11-filter.js
+++ 11-filter.js
@@ -27,8 +27,44 @@ const servicos = [
   { codigo: 5, tipo: "Instalação", status: "Concluído" },
 ];
 
-const filtrarServicosPendentes = servicos.filter((servico) => {
-  return servico.status === "Pendente";
+//const filtrarServicosPendentes = servicos.filter((servico) => {
+//return servico.status === "Pendente";
+//});
+
+// Desestruturação:
+const filtrarServicosPendentes = servicos.filter(({ status }) => {
+  return status === "Pendente";
 });
 
 console.log(filtrarServicosPendentes);
+
+separador();
+
+// Exemplo 3
+const cursosDesign = cursos.filter((curso) => {
+  return curso.categoria === "Design";
+});
+
+console.log(cursosDesign);
+
+separador();
+
+// Exemplo 4
+const cursosExcetoDesign = cursos.filter(
+  (curso) => curso.categoria !== "Design"
+);
+console.log(cursosExcetoDesign);
+
+separador();
+
+/* Exercício
+Gere um novo array com os cursos que atendam os seguintes critérios: 
+    - Cetegoria Front-End OU Mobile 
+    - Preços acima de 600 */
+
+const cursosSelecionados = cursos.filter(
+  (curso) =>
+    (curso.categoria === "Front-End" || curso.categoria === "Mobile") &&
+    curso.preco > 400
+);
+console.log(cursosSelecionados);
